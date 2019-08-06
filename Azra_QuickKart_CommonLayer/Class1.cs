7d60d1fd68c57d73cf95d648870fd6af18ca32c5
parveenazra110 <parveenazra110@gmail.com>
2019-08-06 22:02:50 +0530

    Created Sub Projects

---
+++ Azra_QuickKart_CommonLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azra_QuickKart_CommonLayer
+{
+    public class Class1
+    {
+    }
+}
